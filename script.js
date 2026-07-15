e1d15217ac8ce4eb0bf0bc12afa5019d599a0e43
Tanuja Chuphal <tanujachuphal7@gmail.com>
2026-07-15 20:14:01 +0530

    Added script.js

--- script.js
+++ script.js
@@ -108,27 +108,89 @@
 
 //                Calculate . Add , Sub , Mul , Div
 
-function add(a,b){
-    return a+b;
-}
- function sub(a,b){
-     return a-b;
- }
- function mul(a,b){
-    return a*b;
- }
- function div(a,b){
-    return a/b;
- }
+// function add(a,b){
+//     return a+b;
+// }
+//  function sub(a,b){
+//      return a-b;
+//  }
+//  function mul(a,b){
+//     return a*b;
+//  }
+//  function div(a,b){
+//     return a/b;
+//  }
 
-function calculate(operator, a, b){
-    if(operator ==="+") return add(a,b);
-    if(operator ==="-") return sub(a,b);
-    if(operator ==="*") return mul(a,b);
-    if(operator ==="/") return div(a,b);
-    return "Unknown operator";
-}
-console.log(calculate("+" , 3,4));
-console.log(calculate("-" , 13,4));
-console.log(calculate("*" , 10,4));
-console.log(calculate("/" , 12,4));
+// function calculate(operator, a, b){
+//     if(operator ==="+") return add(a,b);
+//     if(operator ==="-") return sub(a,b);
+//     if(operator ==="*") return mul(a,b);
+//     if(operator ==="/") return div(a,b);
+//     return "Unknown operator";
+// }
+// console.log(calculate("+" , 3,4));
+// console.log(calculate("-" , 13,4));
+// console.log(calculate("*" , 10,4));
+// console.log(calculate("/" , 12,4));
+
+
+// 15-07
+// const fruits =["apple","banana"];
+
+
+//                          foreach , map , filter, find , reduce
+
+//                foreach
+// fruits.forEach((fruits) => {
+//     console.log("print:",fruits);
+// });
+
+const scores = [72,34,56,78,90];
+
+//                  map
+// const data = scores.map((score) => (score > 72 ? "PASS" : "FAIL"));
+// console.log(data);
+
+//                filter
+// const passingScores = scores.filter((score) => score > 72);
+// console.log(passingScores);
+
+//          find
+// const findMethod = scores.find((score) => score > 72);
+// console.log(findMethod);
+
+//                     reduce
+// const total = scores.reduce((sum , score) => sum + score,0);
+// console.log(total);
+
+const students = [
+  {
+    name: "Tanuja",
+    attendance: 20,
+    deliverable: 10,
+    isEligible: true,
+  },
+  {
+    name: "Ankit",
+    attendance: 18,
+    deliverable: 8,
+    isEligible: false,
+  },
+];
+
+students[0].grade = "Merit";
+
+console.log(students[0].isEligible);
+console.log(students);
+console.log(students[0].name);
+
+
+console.log(JSON.stringify(students));
+const studentsJSON = JSON.stringify(students);
+console.log(JSON.parse(studentsJSON));
+
+
+//                    DOM 
+// localStorage.setItem("userName", "Tanuja");
+// console.log(localStorage.getItem(userName));
+
